80cc7ea9ab77297c72817f04df08a8c56898ceb4
xTeKc <bcd3v@pm.me>
2021-10-09 22:22:07 -0400

    remove reqwest and use ureq instead

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use notify::*;
 
 
-fn req() {
-
+fn req() -> Result<(), ureq::Error> {
+    let body: String = ureq::get("https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd")
+        .set("Example-Header", "header value")
+        .call()?
+        .into_string()?;
+    Ok(())
 }
 
 fn main() {
