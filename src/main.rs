a8a2c41f6979f6f7d99f93a58918cda1a14a6c08
xTeKc <bcd3v@pm.me>
2021-10-09 21:58:40 -0400

    make request and call func

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use http::{Request, Response};
 
 
 fn req() {
-    "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd"
-}
+    let request = Request::builder()
+    .uri("https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd")
+    .header("User-Agent", "awesome/1.0")
+    .body(())
+    .unwrap();
+    }
+
+
 
 fn main() {
-    println!("Hello, world!");
-}
+    req();
+
