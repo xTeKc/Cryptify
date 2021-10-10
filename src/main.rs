3d12fc2fa619ff6ca42ddef9c6d12ff662014576
xTeKc <bcd3v@pm.me>
2021-10-09 22:30:30 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use notify::*;
+use ureq;
 
 
-fn req() -> Result<(), ureq::Error> {
-    let body: String = ureq::get("https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd")
-        .set("Example-Header", "header value")
-        .call()?
-        .into_string()?;
-    Ok(())
+fn req()-> Result<(), ureq::Error> {
+    let req: String = ureq::get("https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd")
+    .set("Example-Header", "header value")
+    .call()?
+    .into_string()?;
+    println!("{:?}", req);
+  Ok(())
 }
 
 fn main() {
-    req();
+    let r = req();
+    println!("{:?}", r);
 }
+
+
+
+
