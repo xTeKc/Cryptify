6efcc87a71e2160bf8bb685648215b7575bf62a4
xTeKc <bcd3v@pm.me>
2021-10-09 22:08:21 -0400

    clear request func

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,8 @@ use http::{Request, Response, StatusCode};
 
 
 fn req() {
-    let request = Request::builder()
-    .uri("https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd")
-    .header("User-Agent", "awesome/1.0")
-    .body(())
-    .unwrap();
-    println!("{:?}", request);
-    }
+
+}
 
 fn main() {
     req();
