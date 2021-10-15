1427eb3f822bd671f55a0b7dd8e3c4126ec352ba
xTeKc <bcd3v@pm.me>
2021-10-15 18:34:36 -0400

    comment out for now

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-use notify::*;
-use ureq;
+// use notify::*;
+// use ureq;
+// use std::error::Error;
+// use serde::Deserialize;
 
+// #[derive(Deserialize, Debug)]
+// pub struct Cryptos {
+//   cryptos: Vec<Crypto>
+// }
 
-fn req()-> Result<(), ureq::Error> {
-    let req: String = ureq::get("https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd")
-    .set("Example-Header", "header value")
-    .call()?
-    .into_string()?;
-    println!("{:?}", req);
-  Ok(())
-}
+// #[derive(Deserialize, Debug)]
+// pub struct Crypto {
+//   // name: String,
+//   symbol: String,
+//   marketcap: u32
+// }
+
+// fn crypto_req(req: &str) -> Result<Cryptos, Error> {
+//     let resp = ureq::get(req).call()?.into_string()?;
+//     let cryptos: Cryptos = serde_json::from_str(&resp)?;
+
+//     dbg!(cryptos);
+  
+//     todo!()
+//   }
+
+// fn main() {
+//     let req = "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd";
+//     let cryptos = crypto_req(req);
+//     dbg!(cryptos);
+// }
 
 fn main() {
-    let r = req();
-    println!("{:?}", r);
+    
 }
-
-
-
-
