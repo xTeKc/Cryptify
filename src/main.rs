3db5f47bf468b484f4a84b56bc50a774f45ffd2a
xTeKc <bcd3v@pm.me>
2021-11-04 16:08:36 -0400

    add ui file

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ pub struct Crypto {
   marketcap: u32
 }
 
-fn crypto_req(req: &str) -> Result<Cryptos> {
+fn crypto_req(req: &str) -> Result<Cryptos, Error> {
     let resp = ureq::get(req).call()?.into_string()?;
     let cryptos: Cryptos = serde_json::from_str(&resp)?;
 
