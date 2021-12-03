e507cfcafff6e294cd07175fc9edfec9addef318
xTeKc <bcd3v@pm.me>
2021-12-03 15:55:31 -0500

    comment out

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
-use notify::*;
-use ureq;
-use std::error::Error;
-use serde::Deserialize;
+// use notify::*;
+// use ureq;
+// use std::error::Error;
+// use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-pub struct Cryptos {
-  cryptos: Vec<Crypto>
-}
+// #[derive(Deserialize, Debug)]
+// pub struct Cryptos {
+//   cryptos: Vec<Crypto>
+// }
 
-#[derive(Deserialize, Debug)]
-pub struct Crypto {
-  // name: String,
-  symbol: String,
-  marketcap: u32
-}
+// #[derive(Deserialize, Debug)]
+// pub struct Crypto {
+//   // name: String,
+//   symbol: String,
+//   marketcap: u32
+// }
 
-fn crypto_req(req: String) -> Result<Cryptos, dyn Error> {
-    let resp = ureq::get(req).call()?.into_string()?;
-    let cryptos: Cryptos = serde_json::from_str(&resp)?;
+// fn crypto_req(req: String) -> Result<Cryptos, dyn Error> {
+//     let resp = ureq::get(req).call()?.into_string()?;
+//     let cryptos: Cryptos = serde_json::from_str(&resp)?;
 
-    dbg!(cryptos);
+//     dbg!(cryptos);
   
-    // todo!()
-    Ok(())
-  }
+//     // todo!()
+//     Ok(())
+//   }
 
-//   enum Result<T, E> {
-//     Ok(T),
-//     Err(E),
-//  }
+// //   enum Result<T, E> {
+// //     Ok(T),
+// //     Err(E),
+// //  }
 
-fn main() {
-    let req = "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd";
-    let cryptos = crypto_req(req);
-    dbg!(cryptos);
-}
+// fn main() {
+//     let req = "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=usd";
+//     let cryptos = crypto_req(req);
+//     dbg!(cryptos);
+// }
